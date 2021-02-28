20bfcffe60dd82e5b203921036ea74308616b3ef
kimtaehyung <taehyeongkim77@gmail.com>
2021-02-28 10:03:05 +0900

    4-edit

--- 4/hw3d/WinMaincpp.cpp
+++ 4/hw3d/WinMaincpp.cpp
@@ -11,6 +11,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	case WM_CLOSE:
 		PostQuitMessage(69);
 		break;
+	case WM_KEYDOWN:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, "Respects");
+		}
+		break;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
